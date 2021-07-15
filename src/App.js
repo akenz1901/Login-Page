1618b6e48c9a5e7970d69787da14225099d209f6
akenz1901 <michael_aka1@icloud.com>
2021-07-15 16:36:08 +0100

    App Component is ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Button from './Button';
+import Input from  './Input'
+import './App.css'
+import './Input.css'
+import './Button.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'main'>
+    <div className ="login">
+        <h3>Welcome <br/> Back!</h3>
+        <Input label ="Email" placeholder="Enter Email" id="Email"/>
+        <Input label ="Password" placeholder="Password" id="password"/>
+        <p className="tag"> <u><a href=''>forgot password</a></u></p>
+        <Button Button="LOGIN" backgroundColor="gold"/>
+        <p className="or">or</p>
+        <Button Button="GOOGLE" backgroundColor="red" height = ""/>
+        <Button Button="FACEBOOK" backgroundColor="blue" style=""/>
+    </div>
     </div>
   );
 }
-
 export default App;
