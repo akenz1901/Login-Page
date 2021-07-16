72dca62e58da44f9b98ee9e8d6a95467aa129f49
akenz1901 <michael_aka1@icloud.com>
2021-07-16 19:59:04 +0100

    worked on input

--- src/Input.js
+++ src/Input.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Input = (props) =>{
     return(
         <div ClassName = "signin">
-            <label htmlFor="fname">{props.label}</label><br/>
+            <label htmlFor="fname" className={props.ClassName}>{props.label}</label><br/>
             <input type ="text" id="fname" name="fname" placeholder={props.placeholder}/>   
         </div>
     )
